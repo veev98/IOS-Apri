5ad7f0f79523e1702bb7e0723ff94ae9be432679
veev98 <vikaram98@gmail.com>
2021-06-26 22:00:40 +0800

    Initial Commit

---
+++ Apri/ApriApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApriApp.swift
+//  Apri
+//
+//  Created by Vikaram on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ApriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apri
+//
+//  Created by Vikaram on 26/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
